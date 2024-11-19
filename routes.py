46c7c29fb49bd3b323a8a9c9f9d6564f247d2928
Sandra Ole <sandra.ole@hotmail.fi>
2024-11-19 19:04:39 +0200

    Add routes.py

---
+++ routes.py
@@ -0,0 +1,17 @@
+from app import app
+from flask import render_template, request
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/add_book", methods=["GET", "POST"])
+def add_book():
+    if request.method == "POST":
+        title = request.form["title"]
+        author = request.form["author"]
+        year = request.form["year"]
+        publisher = request.form["publisher"]
+        ISBN_number = request.form["ISBN_number"]
+
+    return render_template(?.html) # päätä mikä html, index.html vai add_book.html
