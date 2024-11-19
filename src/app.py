46e48b8b7adb7da2444265a18525a0b66ec6aa36
Ilari Ranin <raninilari@gmail.com>
2024-11-19 21:27:59 +0200

    from app.py route to routes.py

--- src/app.py
+++ src/app.py
@@ -1,7 +1,8 @@
 from flask import Flask
+from os import getenv
+
 
 app = Flask(__name__)
+app.secret_key = getenv("SECRET_KEY")
 
-@app.route("/")
-def index():
-    return "Heipparallaa!"
+import routes
