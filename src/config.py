e57c38503cadab7d2ff42ae4a298ff960d334e02
lowdodo <lowdodo@ad.helsinki.fi>
2024-11-20 19:24:20 +0200

    flask moved to config

--- src/config.py
+++ src/config.py
@@ -5,11 +5,14 @@ from dotenv import load_dotenv
 from os import getenv
 
 load_dotenv()
+db = SQLAlchemy()
 
 test_env = getenv("TEST_ENV") == "true"
 print(f"Test environment: {test_env}")
 
-app = Flask(__name__)
-app.secret_key = getenv("SECRET_KEY")
-app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
-db = SQLAlchemy(app)
+def create_app():
+    app = Flask(__name__)  
+    app.secret_key = getenv("SECRET_KEY")
+    app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
+    db.init_app(app) 
+    return app
