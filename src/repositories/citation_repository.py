46499cbf06d72ee3be6aa136238a9a6fb3bfc583
lowdodo <lowdodo@ad.helsinki.fi>
2024-11-20 19:26:59 +0200

    added bitrepo olio

--- src/repositories/citation_repository.py
+++ src/repositories/citation_repository.py
@@ -1,28 +1,32 @@
-from db import db
+from config import db
 from sqlalchemy import text
+from flask_sqlalchemy import SQLAlchemy
 
 
-def add_book_citation(title, author, year, publisher, editor, volume, pages, month, isbn, note):
-    sql = text("""
-        INSERT INTO book_citation (title, author, year, publisher, editor, volume, pages, month, isbn, note) 
-        VALUES (:title, :author, :year, :publisher, :editor, :volume, :pages, :month, :isbn, :note)
-    """)
-    db.session.execute(sql, {
-        "title": title,
-        "author": author,
-        "year": year,
-        "publisher": publisher,
-        "editor": editor,
-        "volume": volume,
-        "pages": pages,
-        "month": month,
-        "isbn": isbn,
-        "note": note
-    })
-    db.session.commit()
+class BibtexRepo:
+    def __init__(self, database: SQLAlchemy):
+        self._db = database
 
-def get_all_book():
-    sql = text("SELECT * FROM book_citation")
-    result = db.session.execute(sql)
-    return result.fetchall()
+    def add_book_citation(self,title, author, year, publisher, editor, volume, pages, month, isbn, note):
+        sql = text("""
+            INSERT INTO book_citation (title, author, year, publisher, editor, volume, pages, month, isbn, note) 
+            VALUES (:title, :author, :year, :publisher, :editor, :volume, :pages, :month, :isbn, :note)
+        """)
+        db.session.execute(sql, {
+            "title": title,
+            "author": author,
+            "year": year,
+            "publisher": publisher,
+            "editor": editor,
+            "volume": volume,
+            "pages": pages,
+            "month": month,
+            "isbn": isbn,
+            "note": note
+        })
+        db.session.commit()
 
+    def get_all_books(self):
+        sql = text("SELECT * FROM book_citation")
+        result = self._db.session.execute(sql)
+        return result.fetchall()
