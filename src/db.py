c136655a4df24ca5283378c6d7d4f9017b0d9b42
lowdodo <lowdodo@ad.helsinki.fi>
2024-11-20 19:24:40 +0200

    dell unneccessary

--- src/db.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask, session
-from flask_sqlalchemy import SQLAlchemy
-from os import getenv
-from app import app
-
-app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-db = SQLAlchemy()
