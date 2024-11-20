2890fdb46ed2cef77b13a15dc93f8800b125a7ec
Iida Häkkinen <iida.hakkinen@gmail.com>
2024-11-20 20:15:28 +0200

    fix flash messages

--- src/routes.py
+++ src/routes.py
@@ -20,26 +20,26 @@ def add_book():
 
         if len(title) < 1 or len(title) > 25:
             flash("Title must have 1-25 characters")
-            return render_template(index.html)
+            return render_template("index.html")
         
         if len(author) < 1 or len(author) > 25:
             flash("Author must have 1-25 characters")
-            return render_template(index.html)
+            return render_template("index.html")
         
         if not year.strip():
             flash("Year is required")
-            return render_template(index.html)
+            return render_template("index.html")
         
         if not year.isdigit() or int(year) < 0:
             flash("Year must be a positive number")
         
         if len(publisher) < 1 or len(publisher) > 25:
             flash("Publisher must have 1-25 characters")
-            return render_template(index.html)
+            return render_template("index.html")
         
         if not ISBN_number.strip():
             flash("ISBN_number is required")
-            return render_template(index.html)
+            return render_template("index.html")
         
         if not ISBN_number.isdigit() or len(ISBN_number) < 13:
             flash("ISBN number must be 13 digits")
